d6e7ccf5972401d631c298e87dff9eb657f8d9b7
João Victor <jvfernandes.contato@gmail.com>
2022-04-13 18:52:04 -0300

    utilizando expressões JS no JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 
+const category = 'Posts da Semana';
+
 function App() {
     return (
         <>
-        <h1>Componente App</h1>
-        <h2>Subtitulo</h2>
+        <h1>JStack's Blog</h1>
+        <h2>{category}</h2>
         </>
     );
 }
